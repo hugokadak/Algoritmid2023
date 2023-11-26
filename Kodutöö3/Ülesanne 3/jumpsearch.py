56d0a3fd1f058d56b6d6e0df4fc45187e82e4302
hugokadak <119073767+hugokadak@users.noreply.github.com>
2023-11-26 15:58:44 +0200

    remove py file
    tegin selle aga poole peal sain aru et ülesanne ei nõudnud programmi

--- Kodutöö3/Ülesanne 3/jumpsearch.py
+++ Kodutöö3/Ülesanne 3/jumpsearch.py
@@ -1,17 +1,1 @@
-import math
-num_arr = [1, 2, 3, 5, 7, 12, 19, 31, 50, 81, 131, 212]
-
-def jump_search(array, num):
-    bmi = len(num_arr)
-    roblox = math.sqrt(bmi)
-    print(str(bmi), str(roblox))
-    alg = 0
-    pisipisi = int(min(roblox, bmi))
-    while array[pisipisi-1] < x :
-    
-    return num
-
-
-leitav = 50
-#leitav = int(input("Sisesta leitav:"))
-print(jump_search(num_arr, leitav))
+-
